b3605e32cbb169c4cbd2242a85af74277801507a
parkjangwon <vim@kakao.com>
2026-03-15 17:19:24 +0900

    chore: SQLite용 마이그레이션 재생성

--- prisma/migrations/20260315030000_add_requesttype_categoryid/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "RequestType" ADD COLUMN "categoryId" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "RequestType_categoryId_key" ON "RequestType"("categoryId");
-
--- AddForeignKey
-ALTER TABLE "RequestType" ADD CONSTRAINT "RequestType_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
